2a9fb30a863a2b815735b9ffa8bda70a8ab18b89
drive <drive@driveline24>
2025-05-26 02:34:55 +0900

    2-20 クラスの基本

--- HelloWorld_Koki/src/basicClass/Animal.java
+++ HelloWorld_Koki/src/basicClass/Animal.java
@@ -1,13 +1,35 @@
 package basicClass;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Animal {
 	
 	public static void main(String[] args) {
 		// Q1：Dogクラスを呼び出して変数に代入し、動物の名前をコンソールに出力してください。
 		
+		// コンストラクタ呼び出し
+		Dog dogName = new Dog();
+		// コンソール出力
+		System.out.println(dogName.name);
+		
 		// Q2：Dogクラスを呼び出して変数に代入し、動物の数をコンソールに出力してください。
+		
+		// 数を指定してコンストラクタ呼び出し
+		Dog dogCount = new Dog(3);
+		// コンソール出力
+		System.out.println(dogCount.count);
 		
 		// 現在の日時を「yyyy-MM-dd H:m:s」形式で変数に代入して、コンソールに出力してください
 		
+		// 現在の日時を取得
+		LocalDateTime now = LocalDateTime.now();
+		// 形式指定
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd H:m:s");
+		
+		// 形式を適用して文字列に変換
+		String formatDate = now.format(formatter);
+		// コンソール出力
+		System.out.println(formatDate);
 	}
 }
--- HelloWorld_Koki/src/basicClass/Dog.java
+++ HelloWorld_Koki/src/basicClass/Dog.java
@@ -3,10 +3,22 @@ package basicClass;
 public class Dog {
 	// Q1：フィールドに動物の名前の変数を定義してください。
 	
+	public String name;
+	
 	// Q2：フィールドに動物の数の変数を定義してください。
+	
+	public int count;
 	
 	// Q3：Q1で作成した変数に「犬」を代入するコンストラクタを作成してください。
 	
+	public Dog() {
+		this.name = "犬";
+	}
+	
 	// Q4：Q2で作成した変数に引数を代入するコンストラクタを作成してください。
+	
+	public Dog(int count) {
+		this.count = count;
+	}
 	
 }
